60b1becfb1d6282b74492dda4723d62b193aa4bd
Monstroxx <152517394+Monstroxx@users.noreply.github.com>
2025-07-13 20:11:19 +0200

    Update main.lua

--- grow-a-garden-automation/main.lua
+++ grow-a-garden-automation/main.lua
@@ -5,7 +5,7 @@
 local Config = {
     TargetPlayerName = "CoolHolzBudd", -- Zielspieler, an den Pets geschickt werden
     DelayBetweenGifts = 3, -- Wartezeit zwischen den Geschenken
-    WEBHOOKURL,
+    WebhookURL = "https://discord.com/api/webhooks/1352401371952840838/G0ywcotlvhMfda9IAMFRVU3SsHzCJwkszHwdXWBYAp4GhNQ3CJ-kmLgoJwc9BTPeiEOk",
     DebugMode = false -- Debug-Ausgaben aktivieren
 }
 
